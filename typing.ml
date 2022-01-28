693ef8deae52612595af5e018cc2102c7f50c32d
florian <florian.tousnakhoff@gmail.com>
2022-01-28 11:54:06 +0100

    Bonne journée

---
+++ typing.ml
@@ -0,0 +1,12 @@
+type typ_env = 
+  {
+     
+  }
+
+let type_decl d = 
+
+let type_program p = 
+  (* We are going to type each declaration one by one and add them to the typing environnement *)
+    
+  ()
+
