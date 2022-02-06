d7b1f0d82ddeb59ef8e292557e0a729c357e1ce2
floriantsf <florian.tousnakhoff@ens.psl.eu>
2022-02-06 15:43:55 +0100

    mkMul en cours

--- instruction_selection.ml
+++ instruction_selection.ml
@@ -12,6 +12,29 @@ let rec mkAdd e1 e2 = match (e1, e2) with (* mkAdd prends en entrées des expres
   |_ -> Sel_binop (Iadd, e1, e2) (* Si pas d'optimisation possible *)
 
 let rec mkSub e1 e2 = match (e1, e2) with
+  |(Sel_Eint n1, Sel_Eint n2) ->  Sel_Eint (n1 - n2)
+  |(Sel_Eint 0, e2) ->  mkNeg e2
+  |(e1, Sel_Eint 0) -> e1
+  |(Sel_binop (Iadd, Sel_Eint n1, e) , Sel_Eint n2) -> mkAdd (Sel_Eint (n1-n2)) e  
+  (*|(Sel_Eint n, e) -> Sel_unop (IUadd n, e) *)
+  |(e, Sel_Eint n) -> Sel_unop (IUsub n ,e)
+  |_ -> Sel_binop (Isub, e1, e2) (* Si pas d'optimisation possible *)
+
+let pure e = match e with (* Determine si l'expression est pure *)
+  |Ty_Eint _ |Ty_Eident _ -> true
+  |Ty_Eunop (_, e1) -> pure e1
+  |Ty_Ebinop (_, e1, e2) -> pure e1 && pure e2
+  |Ty_Eassign_var _ |Ty_Eassign_ch -> false
+  |Ty_Ecall _ -> false
+  |Ty_Ept -> true
+let rec mkMul e1 e2 = match (e1, e2) with
+  |(Sel_Eint n1, Sel_Eint n2) ->  Sel_Eint (n1*n2)
+  |(Sel_Eint 0, e2) ->  mkNeg e2
+  |(e1, Sel_Eint 0) -> e1
+  |(Sel_binop (Iadd, Sel_Eint n1, e) , Sel_Eint n2) -> mkAdd (Sel_Eint (n1-n2)) e  
+  (*|(Sel_Eint n, e) -> Sel_unop (IUadd n, e) *)
+  |(e, Sel_Eint n) -> Sel_unop (IUsub n ,e)
+  |_ -> Sel_binop (Iimul, e1, e2) (* Si pas d'optimisation possible *)
 
 let instr_selec e = match e with (* Fait la selection d'instructions *)
   |Ty_Eint n -> Sel_Eint n 
@@ -20,6 +43,8 @@ let instr_selec e = match e with (* Fait la selection d'instructions *)
   |Ty_Ebinop (op, e1, e2) -> 
     begin match op with
       |Badd -> mkAdd (instr_selec e1) (instr_selec e2)
+      |Bsub -> mkSub (instr_selec e1) (instr_selec e2)
+      |Bmul -> mkMul (instr_selec e1) (instr_selec e2)
       |_ -> assert false
     end
   |Ty_Eassign_var (s, e1) -> 
