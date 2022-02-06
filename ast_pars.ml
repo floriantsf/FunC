cff17add05265199a1824a67adcadfa7f8e1bf81
Theotime <theotime.lh@gmail.com>
2022-02-06 21:06:59 +0100

    ajoute les int x = 5 ; dans les ast

--- ast_pars.ml
+++ ast_pars.ml
@@ -6,39 +6,34 @@ exception Parser_error of string
 
 (* dv : decl_vars ; dt : decl_typ, ; df : decl_fct *)
 
-type par_dv = {typ : ctype desc ; vars : ident desc list}
-
 type par_expr =
   | Par_Eint of int
   | Par_Eident of ident
   | Par_Eunop of unop * (par_expr desc)
   | Par_Ebinop of binop * (par_expr desc) * (par_expr desc)
   | Par_Ecall of (ident desc) * (par_expr desc list)
-  | Par_Ept of (expr desc) * (ident desc)
+  | Par_Ept of (par_expr desc) * (ident desc)
   | Par_Esize of ident desc
 
 type par_stmt =
+  | Par_Sdv of (ctype desc) * (((ident desc) * (par_expr desc option)) list)
   | Par_Snil
   | Par_Sexpr of par_expr desc
   | Par_Sif of (par_expr desc) * (par_stmt desc) * (par_stmt desc)
   | Par_Swhile of (par_expr desc) * (par_stmt desc)
-  | Par_Sbloc of par_bloc
+  | Par_Sbloc of par_stmt desc list
   | Par_Sreturn of par_expr desc
-
-and par_bloc_un =
-  | Par_Bdv of par_dv
-  | Par_Bstmt of par_stmt
-and par_bloc = par_bloc_un desc list
 
 type par_param = {typ : ctype desc ; nom : ident desc}
 
+type par_dv = {typ : ctype desc ; vars : ident desc list}
 type par_dt = {nom : ident desc ; fields : par_dv list}
 
 type par_df = 
 { type_r : ctype desc ; 
   nom : ident desc ;
   params : par_param list ;
-  body : bloc desc }
+  body : par_bloc }
 
 type par_decl = 
   | Par_Ddt of par_dt
--- ast_ty.ml
+++ ast_ty.ml
@@ -6,8 +6,6 @@ exception Typing_error of {loc : loc ; msg : string}
 
 (* dv : decl_vars ; dt : decl_typ, ; df : decl_fct *)
 
-type ty_dv = {typ : ctype ; var : ident}
-
 (* We didn't need decl_type anymore *)
 
 type ty_expr = 
@@ -21,22 +19,18 @@ type ty_expr =
   | Ty_Ept of expr * int
  
 type ty_stmt =
+  | Ty_Sdv of ident * (ty_expr option)
   | Ty_Snil
   | Ty_Sexpr of ty_expr
   | Ty_Sif of ty_expr * ty_stmt * ty_stmt
   | Ty_Swhile of ty_expr * ty_stmt
-  | Ty_Sbloc of ty_bloc
+  | Ty_Sbloc of ty_stmt list
   | Ty_Sreturn of ty_expr
 
-and ty_bloc_un = 
-  | Ty_Bdv of ty_dv
-  | Ty_Bstmt of ty_stmt
-and ty_bloc = ty_bloc_un list
-   
 type ty_df =
 { id : ident ;
   params : ident list ;
-  body : ty_bloc }
+  body : ty_stmt list }
 
 type ty_file = ty_df list
  
